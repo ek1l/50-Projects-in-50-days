8d7d397ab5ff8d19ec34ba8ede740f1c76839717
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-05-25 09:15:33 -0300

    ...

--- 9 - Sound Board/index.js
+++ 9 - Sound Board/index.js
@@ -1,5 +1,6 @@
 const applause = document.querySelector('#applause')
 const boo = document.querySelector('#boo')
+const gasp = document.querySelector("#gasp")
 
 applause.addEventListener('click', () => {
     const audio = document.querySelector('#tocarApplause')
@@ -10,6 +11,13 @@ applause.addEventListener('click', () => {
 
 boo.addEventListener('click', () => {
     const audio = document.querySelector('#tocarBoo')
+    audio.currentTime = 0.2
+    audio.play()
+})
+
+
+gasp.addEventListener('click', () => {
+    const audio = document.querySelector('#tocarGasp')
     audio.currentTime = 0.2
     audio.play()
 })
