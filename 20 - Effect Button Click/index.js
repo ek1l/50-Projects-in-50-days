3b324b202eb8256711fe8265121409004a74f91a
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-06-06 16:08:46 -0300

    ...

--- 20 - Effect Button Click/index.js
+++ 20 - Effect Button Click/index.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll(".ripple");
 
 buttons.forEach((button) => {
-  button.addEventListener("click", function(e) {
+  button.addEventListener("click", function (e) {
     const x = e.clientX;
     const y = e.clientY;
 
@@ -11,12 +11,12 @@ buttons.forEach((button) => {
     const xInside = x - buttonLeft;
     const yInside = y - buttonTop;
 
-    const circle = document.createElement('span')
-    circle.classList.add('circle')
-    circle.style.top = yInside + 'px'
-    circle.style.left = xInside + 'px'
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = yInside + "px";
+    circle.style.left = xInside + "px";
 
-    this.appendChild(circle)
-    setTimeout(() => circle.remove(), 500)
+    this.appendChild(circle);
+    setTimeout(() => circle.remove(), 500);
   });
 });
